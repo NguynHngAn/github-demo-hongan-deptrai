3603264c223f96776a706b76fb33bbed4dbc1bdc
Ank <nguyenhongan.tphcm@gmail.com>
2024-06-17 09:17:35 +0700

    Add About.cs

---
+++ Github-Demo/About.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Github_Demo
+{
+    abstract class About
+    {
+        abstract void Speak();
+    }
+    class Cat : About
+    {
+        public override void Speak() 
+        {
+            Console.WriteLine("Meo meo...");
+        }
+}
