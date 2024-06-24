e9120d3c13a1a27f16e67d3fdcf210c6bded7004
Ank <nguyenhongan.tphcm@gmail.com>
2024-06-24 16:14:10 +0700

    add 2 file Infomation + ListJav

---
+++ Github-Demo/Infomation/Animal.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Github_Demo.Infomation
+{
+    public abstract class Animal
+    {
+        public abstract void Speak();
+    }
+
+    public class Dog : Animal
+    {
+        public override void Speak() =>
+            Console.WriteLine("Gau gau");
+    }
+}
---
+++ Github-Demo/ListJav/ListJav.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Github_Demo.ListJav
+{
+    public static class ListJav
+    {
+        public static void ListView()
+        {
+            Console.WriteLine("This is An's jav list");
+        }
+    }
+}
+
